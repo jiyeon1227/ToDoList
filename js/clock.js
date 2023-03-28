43d92d1f1e1b6183d08d3a8c151026146a5b1709
jiyeon1227 <74654224+jiyeon1227@users.noreply.github.com>
2023-03-28 20:05:13 +0900

    Add files via upload

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0"); // padStart()는 내가 가지고 있는 string보다 길게 만들어야 할때 사용(string앞에 문자를 넣음)
+    const minutes = String(date.getMinutes()).padStart(2, "0"); //(2, "0")는 두자리수인데 문자가 없으면 앞에 0을 넣는다는뜻
     const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 
-getClock();
-setInterval(getClock,1000);
+getClock(); // 최초 한번 실행
+setInterval(getClock,1000); // 1초마다 getcolock를 실행
